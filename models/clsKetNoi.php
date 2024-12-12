fe660943cbc9644a3ee4a0a9f851fa11ecf7390a
UyenQuyen <uynwiin@gmail.com>
2024-12-13 01:40:56 +0700

    Fix conflict

--- models/clsKetNoi.php
+++ models/clsKetNoi.php
@@ -3,12 +3,7 @@ class clsKetNoi
 {
     private $conn;
 
-<<<<<<< HEAD
     function ketNoiDB() {
-=======
-    function ketNoiDB()
-    {
->>>>>>> 7ed8cb8294fd34bb0c2864f6de717da1af4b4283
         $this->conn = new mysqli("localhost", "root", "", "ptud");
 
         // Kiểm tra kết nối
